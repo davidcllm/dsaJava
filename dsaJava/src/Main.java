04c8e7652a90b182596bfda2b2a48f79afedc222
davidcllm <davcm24@hotmail.com>
2026-01-11 23:38:20 -0600

    Stacks
    Se construyó un Stack.

---
+++ dsaJava/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        //stack: LIFO, is likee a pile of plates
+
+        Stack<String> stack = new Stack<String>();
+        //System.out.println(stack.empty());
+
+        stack.push("Minecraft");
+        stack.push("Skyrim");
+        stack.push("DOOM");
+        stack.push("Borderlands");
+        stack.push("FFVII");
+
+        //System.out.println(stack.empty());
+    }
+}
