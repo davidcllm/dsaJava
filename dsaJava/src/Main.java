8774deca47b22bd0c1fb7119536f3ce9aea6e02a
davidcllm <davcm24@hotmail.com>
2026-01-14 01:03:14 -0600

    Declaration [Queues]

--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -1,25 +1,11 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        //stack: LIFO, is likee a pile of plates
+        //queue: FIFO structure
 
-        Stack<String> stack = new Stack<String>();
-        //System.out.println(stack.empty());
-
-        stack.push("Minecraft");
-        stack.push("Skyrim");
-        stack.push("DOOM");
-        stack.push("Borderlands");
-        stack.push("FFVII");
-
-        //System.out.println(stack.empty());
-        System.out.println(stack);
-
-        /*String myFavGame = stack.pop();
-        System.out.println(myFavGame);*/
-
-        System.out.println(stack.peek());
-        System.out.println(stack.search("DOOM"));
+        Queue<String> queue = new LinkedList<String>();
     }
 }
