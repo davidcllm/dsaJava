ee8e795de745d6e1e4d504e7db5b7cfae8b0c287
davidcllm <davcm24@hotmail.com>
2026-01-15 23:36:23 -0600

    [Priority Queues]

--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -1,29 +1,24 @@
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        //queue: FIFO structure
+        //Priority queue: FIFO structure,
+        // serves the elements with higher priority first
 
-        Queue<String> queue = new LinkedList<String>();
+        Queue<Double> queue = new PriorityQueue<>();
 
-        System.out.println(queue.isEmpty());
+        queue.offer(3.0);
+        queue.offer(2.5);
+        queue.offer(4.0);
+        queue.offer(1.5);
+        queue.offer(2.0);
 
-        queue.offer("Karen");
-        queue.offer("Chad");
-        queue.offer("Steve");
-        queue.offer("Harold");
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
 
-        System.out.println(queue.isEmpty());
-        System.out.println(queue.size());
-        System.out.println(queue.contains("Harold"));
-
-        //System.out.println(queue);
-
-        //System.out.println(queue.peek());
-
-        //queue.poll();
-        //System.out.println(queue);
     }
 }
