5c37938d2282dc0482763253963591c59b46f920
davidcllm <davcm24@hotmail.com>
2026-01-15 23:37:13 -0600

    Reverse Order [Priority Queues]

--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -1,14 +1,11 @@
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         //Priority queue: FIFO structure,
         // serves the elements with higher priority first
 
-        Queue<Double> queue = new PriorityQueue<>();
+        Queue<Double> queue = new PriorityQueue<>(Collections.reverseOrder());
 
         queue.offer(3.0);
         queue.offer(2.5);
