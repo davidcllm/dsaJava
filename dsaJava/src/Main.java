855ee671bce89f1d599234fb5658bcaf71880e4e
davidcllm <davcm24@hotmail.com>
2026-01-17 23:31:57 -0600

    peek [LinkedList]

--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -33,5 +33,8 @@ public class Main {
 
         linkedList.remove("E");
         System.out.println(linkedList);
+
+        System.out.println(linkedList.peekFirst());
+        System.out.println(linkedList.peekLast());
     }
 }
