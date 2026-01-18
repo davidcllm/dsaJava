0be43d6699223eb195de61af5c46c1e4d9d880b4
davidcllm <davcm24@hotmail.com>
2026-01-17 23:26:48 -0600

    Offer y Poll [LinkedList]

--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -6,14 +6,25 @@ public class Main {
 
         LinkedList<String> linkedList = new LinkedList<String>();
 
+        // Como con los stacks
         // con push el orden es: F, D, C, B, A
         // pop elimina F
-        linkedList.push("A");
+        /*linkedList.push("A");
         linkedList.push("B");
         linkedList.push("C");
         linkedList.push("D");
         linkedList.push("F");
-        linkedList.pop();
+        linkedList.pop();*/
+
+        // Como con las queues
+        // con offer el orden es: A, B, C, D, F
+        // poll elimina la A
+        linkedList.offer("A");
+        linkedList.offer("B");
+        linkedList.offer("C");
+        linkedList.offer("D");
+        linkedList.offer("F");
+        linkedList.poll();
 
         System.out.println(linkedList);
 
