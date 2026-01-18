7de9792e2b6d87febee822101251a4b1dfb7d665
davidcllm <davcm24@hotmail.com>
2026-01-17 23:24:12 -0600

    Push y Pop [LinkedList]

--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -5,11 +5,17 @@ public class Main {
         //Linked List
 
         LinkedList<String> linkedList = new LinkedList<String>();
+
+        // con push el orden es: F, D, C, B, A
+        // pop elimina F
         linkedList.push("A");
         linkedList.push("B");
         linkedList.push("C");
         linkedList.push("D");
         linkedList.push("F");
+        linkedList.pop();
+
+        System.out.println(linkedList);
 
     }
 }
