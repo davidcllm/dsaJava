d4f5d4da51d0581a9d504ea55891efe51110dc53
davidcllm <davcm24@hotmail.com>
2026-01-19 01:02:13 -0600

    Creación de la clase [DynamicArray]

--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -4,7 +4,23 @@ public class Main {
     public static void main(String[] args) {
         //Dynamic Arrays
 
-        ArrayList<String> arrayList = new ArrayList<String>();
+        DynamicArray dynamicArray = new DynamicArray(5);
 
+        System.out.println(dynamicArray.capacity);
+
+    }
+}
+
+class DynamicArray {
+    int size;
+    int capacity = 10;
+    Object[] array;
+
+    public DynamicArray() {
+        this.array = new Object[capacity];
+    }
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        this.array = new Object[capacity];
     }
 }
