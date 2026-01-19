d796b6e9cd9e885f41020ba7e320eee4df8a6418
davidcllm <davcm24@hotmail.com>
2026-01-19 00:57:57 -0600

    [ArrayList]

--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -2,48 +2,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        //Linked List: good with insertion and deletion of nodes O1.
-        // stores nodes in 2 parts (data + address)
-        // there are two types singly and doubly linked lists
+        //Dynamic Arrays
 
-        LinkedList<String> linkedList = new LinkedList<String>();
+        ArrayList<String> arrayList = new ArrayList<String>();
 
-        // Como con los stacks
-        // con push el orden es: F, D, C, B, A
-        // pop elimina F
-        /*linkedList.push("A");
-        linkedList.push("B");
-        linkedList.push("C");
-        linkedList.push("D");
-        linkedList.push("F");
-        linkedList.pop();*/
-
-        // Como con las queues
-        // con offer el orden es: A, B, C, D, F
-        // poll elimina la A
-        linkedList.offer("A");
-        linkedList.offer("B");
-        linkedList.offer("C");
-        linkedList.offer("D");
-        linkedList.offer("F");
-        //linkedList.poll();
-
-        linkedList.add(4, "E");
-        System.out.println(linkedList);
-
-        System.out.println(linkedList.indexOf("E"));
-
-        linkedList.remove("E");
-        System.out.println(linkedList);
-
-        System.out.println(linkedList.peekFirst());
-        System.out.println(linkedList.peekLast());
-
-        linkedList.addFirst("0");
-        linkedList.addLast("Z");
-        System.out.println(linkedList);
-
-        String first = linkedList.removeFirst();
-        String last = linkedList.removeLast();
     }
 }
