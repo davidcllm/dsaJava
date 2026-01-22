2b6c6d5a1866e8c402ddf0f2321d26f6d3db3555
davidcllm <davcm24@hotmail.com>
2026-01-22 12:28:04 -0600

    declaración [LL vs AL]

--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -2,116 +2,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        //Dynamic Arrays
+        // LinkedList vs ArrayList
 
-        DynamicArray dynamicArray = new DynamicArray(5);
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
 
-        dynamicArray.add("A");
-        dynamicArray.add("B");
-        dynamicArray.add("C");
-        dynamicArray.add("D");
-        dynamicArray.add("E");
-        dynamicArray.add("F");
+        long startTime;
+        long endTime;
+        long elapsedTime;
 
-        dynamicArray.delete("A");
-        dynamicArray.delete("B");
-        dynamicArray.delete("C");
-
-        System.out.println(dynamicArray);
-        System.out.println("size: " + dynamicArray.size);
-        System.out.println("capacity: " + dynamicArray.capacity);
-        System.out.println("empty: " + dynamicArray.isEmpty());
-    }
-}
-
-class DynamicArray {
-    int size;
-    int capacity = 10;
-    Object[] array;
-
-    public DynamicArray() {
-        this.array = new Object[capacity];
-    }
-    public DynamicArray(int capacity) {
-        this.capacity = capacity;
-        this.array = new Object[capacity];
-    }
-    public void add(Object data) {
-        if (size >= capacity) {
-            grow();
-        }
-        array[size] = data; // array[size] is the end of the array
-        size++;
-    }
-    public void insert(int index, Object data) {
-        if (size >= capacity) {
-            grow();
-        }
-        for (int i = size; i > index; i--) {
-            array[i] = array[i-1]; // Moves the elements to the right to make room for the insertion
-        }
-        array[index] = data;
-        size++;
-    }
-    public void delete(Object data) {
-        for (int i = 0; i < size; i++) {
-            if (array[i] == data) {
-                for (int j = 0; j < (size - i - 1); j++) {
-                    array[i+j] = array[i+j+1]; // Move the elements to the left
-                }
-                array[size-1] = null;
-                size--;
-
-                if (size <= (int) (capacity/3)) {
-                    shrink();
-                }
-                break;
-            }
-        }
-    }
-    public int search(Object data) {
-        for (int i = 0; i < size; i++) {
-            if (array[i] == data) {
-                return i;
-            }
-        }
-        return -1;
-    }
-    private void grow() {
-        int newCapacity = (int)(capacity * 2);
-        Object[] newArray = new Object[newCapacity];
-
-        for (int i = 0; i < size; i++) {
-            newArray[i] = array[i];
-        }
-        capacity = newCapacity;
-        array = newArray;
-    }
-    private void shrink() {
-        int newCapacity = (int)(capacity / 2);
-        Object[] newArray = new Object[newCapacity];
-
-        for (int i = 0; i < size; i++) {
-            newArray[i] = array[i];
-        }
-        capacity = newCapacity;
-        array = newArray;
-    }
-    public boolean isEmpty() {
-        return size == 0;
-    }
-    public String toString() {
-        String string = "";
-        for (int i = 0; i < size; i++) {
-            string += array[i] + ", ";
-        }
-        if (string != "") {
-            string = "[" + string.substring(0, string.length() - 2) + "]";
-        }
-        else {
-            string = "[]";
-        }
-
-        return string;
+        
     }
 }
