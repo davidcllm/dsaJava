ae20fb59e8519a18f25d4d198850c91208315e6a
davidcllm <davcm24@hotmail.com>
2026-03-16 23:38:22 -0600

    algoritmo completo [Interpolation Search]

--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -11,6 +11,12 @@ public class Main {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int index = interpolationSearch(array, 8);
 
+        if (index != -1) {
+            System.out.println("Element found at: " + index);
+        }
+        else {
+            System.out.println("Element not found");
+        }
 
 
     }
@@ -21,10 +27,22 @@ public class Main {
 
         while (value >= array[low] && value <= array[high] && low <= high) {
             int probe = low + (high - low) * (value - array[low]) / (array[high] - array[low]);
+
+            System.out.println("Probe: " + probe);
+
+            if (array[probe] == value) {
+                return probe;
+            }
+            else if (array[probe] < value) {
+                low = probe + 1;
+            }
+            else {
+                high = probe - 1;
+            }
         }
 
 
-        return 0;
+        return -1;
     }
 
 }
