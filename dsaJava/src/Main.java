7820dd3883aac30a16d856b2f455bef2065d40d1
davidcllm <davcm24@hotmail.com>
2026-04-01 23:54:10 -0600

    Ascending [Insertion Sort]

--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -2,33 +2,34 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        //selection sort: searches the min value at each iteration and swaps variables
+        //insertion sort: after comparing elements to the left
+        //shift elements to the right to make room to insert a value
+
         //complexity: O(n^2)
-        //small data set = okay
-        //big data set = very bad
+        //small data set: decent
+        //big data set: bad
+
+        //Best case O(n)
 
         int array[] = {8, 7, 9, 2, 3, 1, 5, 4, 6};
 
-        selectionSort(array);
+        insertionSort(array);
 
         for (int i : array) {
-            System.out.print(i);
+            System.out.print(i + " ");
         }
     }
 
-    private static void selectionSort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            int min = i;
-
-            for (int j = i + 1; j < array.length - 1; j++) {
-                if (array[min] < array[j]) {
-                    min = j;
-                }
-            }
-
+    private static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
             int temp = array[i];
-            array[i] = array[min];
-            array[min] = temp;
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
         }
     }
 }
