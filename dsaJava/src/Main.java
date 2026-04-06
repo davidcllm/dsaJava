4dc56356b6f3d5d36e92a8e1a048db8cf4c6516e
davidcllm <davcm24@hotmail.com>
2026-04-05 23:41:59 -0600

    Power [Recusrion]

--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -6,12 +6,12 @@ public class Main {
         //adv: easier to read/write, easier to debug
         //disad: sometimes slower, uses more memory
 
-
-        System.out.println(factorial(7));
+        System.out.println(power(2, 8));
     }
-    private static int factorial(int num) {
-        if(num < 1) return 1;
-        return num * factorial(num - 1);
+
+    private static int power(int base, int exp) {
+        if (exp < 1) return 1;
+        return base*power(base, exp-1);
     }
 
 }
