269a1b68ad3c1d0c3cd8ab7a87c562afd143aa2f
davidcllm <davcm24@hotmail.com>
2026-04-06 23:58:52 -0600

    [Merge Sort]

--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -2,16 +2,48 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        //recursion: A recursive method calls itself.
-        //adv: easier to read/write, easier to debug
-        //disad: sometimes slower, uses more memory
+        //merge sort
+        //Complexity:
 
-        System.out.println(power(2, 8));
+        int[] array = {8, 2, 5, 3, 4, 7, 6, 1};
+
+        mergeSort(array);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
+        }
     }
 
-    private static int power(int base, int exp) {
-        if (exp < 1) return 1;
-        return base*power(base, exp-1);
+    private static void mergeSort(int[] array) {
+        int length = array.length;
+        if (length <= 1) return;
+
+        int middle = length / 2;
+        int[] leftArray = new int[middle];
+        int[] rightArray = new int[middle];
+
+        int i = 0; //left
+        int j = 0; //right
+
+        for (; i < length; i++) {
+            if (i < middle) {
+                leftArray[i] = array[i];
+            }
+            else {
+                rightArray[i] = array[i];
+                j++;
+            }
+        }
+
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+
+        merge(leftArray, rightArray, array);
     }
+
+    private static void merge(int[] leftArray, int[] rightArray, int[] array) {
+
+    }
+
 
 }
