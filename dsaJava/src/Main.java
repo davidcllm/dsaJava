2d358d52da3cb85045b68ccdbbb464ccf9944187
davidcllm <davcm24@hotmail.com>
2026-04-16 23:52:59 -0600

    String key and Hash Code [Hash Tables]

--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -2,20 +2,20 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Hashtable<Integer, String> table = new Hashtable<>(10);
+        Hashtable<String, String> table = new Hashtable<>(10);
 
-        table.put(100, "Spongebob");
-        table.put(123, "Patrick");
-        table.put(321, "Sandy");
-        table.put(555, "Squidward");
-        table.put(777, "Gary");
+        table.put("100", "Spongebob");
+        table.put("123", "Patrick");
+        table.put("321", "Sandy");
+        table.put("555", "Squidward");
+        table.put("777", "Gary");
 
         //table.remove(777);
 
         System.out.println(table.get(100));
 
-        for (Integer key : table.keySet()) {
-            System.out.println(key.hashCode() % 10 + "\t" + key + "\t" + table.get(key));
+        for (String key : table.keySet()) {
+            System.out.println(key.hashCode() + "\t" + key + "\t" + table.get(key));
         }
     }
 }
