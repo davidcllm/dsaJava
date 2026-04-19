c70986f278316637c85f1de23cbf682d0171ced8
davidcllm <davcm24@hotmail.com>
2026-04-18 23:42:35 -0600

    Estructura Grafo y Nodo [Adjacency Matrix]

---
+++ dsaJava/src/Graph.java
@@ -0,0 +1,22 @@
+public class Graph {
+    int[][] matrix;
+    Graph(int size) {
+        matrix = new int[size][size];
+    }
+
+    public void addNode(Node node) {
+
+    }
+
+    public void addEdge(int src, int dst) {
+
+    }
+
+    public boolean checkEdge(int src, int dst) {
+
+    }
+
+    public void print() {
+        
+    }
+}
--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -2,20 +2,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Hashtable<String, String> table = new Hashtable<>(10);
+        //Adjacency matrix
+        //runtime complexity to check an edge: O(1)
+        //space complexity: O(v^2)
 
-        table.put("100", "Spongebob");
-        table.put("123", "Patrick");
-        table.put("321", "Sandy");
-        table.put("555", "Squidward");
-        table.put("777", "Gary");
 
-        //table.remove(777);
-
-        System.out.println(table.get(100));
-
-        for (String key : table.keySet()) {
-            System.out.println(key.hashCode() + "\t" + key + "\t" + table.get(key));
-        }
     }
 }
+
