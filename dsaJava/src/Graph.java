f9ba778e3ff0e40e55b0e0d123f61de72f4f592f
davidcllm <davcm24@hotmail.com>
2026-04-19 23:27:18 -0600

    addEdge y checkEdge [Adjacency Matrix]

--- dsaJava/src/Graph.java
+++ dsaJava/src/Graph.java
@@ -9,14 +9,19 @@ public class Graph {
     }
 
     public void addEdge(int src, int dst) {
-
+        matrix[src][dst] = 1;
     }
 
     public boolean checkEdge(int src, int dst) {
-
+        if (matrix[src][dst] == 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public void print() {
-        
+
     }
 }
--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -6,6 +6,14 @@ public class Main {
         //runtime complexity to check an edge: O(1)
         //space complexity: O(v^2)
 
+        Graph graph = new Graph(5);
+        graph.addNode(new Node("A"));
+        graph.addNode(new Node("B"));
+        graph.addNode(new Node("C"));
+        graph.addNode(new Node("D"));
+        graph.addNode(new Node("E"));
+
+        graph.addEdge(0, 1);
 
     }
 }
