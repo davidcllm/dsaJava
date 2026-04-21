97f0c291b5bdfb2fefd3e8e813e644396883d88a
davidcllm <davcm24@hotmail.com>
2026-04-20 23:39:15 -0600

    Print Matrix [Adjacency Matrix]

--- dsaJava/src/Graph.java
+++ dsaJava/src/Graph.java
@@ -22,6 +22,11 @@ public class Graph {
     }
 
     public void print() {
-
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -14,6 +14,13 @@ public class Main {
         graph.addNode(new Node('E'));
 
         graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(4, 0);
+        graph.addEdge(4, 2);
+
+        graph.print();
 
     }
 }
