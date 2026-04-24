196a9ef54728c0d882c7b1ea00a277c2da25e25d
davidcllm <davcm24@hotmail.com>
2026-04-23 23:55:24 -0600

    formato print [Adjacency Matrix]

--- dsaJava/src/Graph.java
+++ dsaJava/src/Graph.java
@@ -1,11 +1,15 @@
+import java.util.ArrayList;
+
 public class Graph {
+    ArrayList<Node> nodes;
     int[][] matrix;
     Graph(int size) {
+        nodes = new ArrayList<>();
         matrix = new int[size][size];
     }
 
     public void addNode(Node node) {
-
+        nodes.add(node);
     }
 
     public void addEdge(int src, int dst) {
@@ -22,7 +26,14 @@ public class Graph {
     }
 
     public void print() {
+        System.out.print("  ");
+        for (Node node : nodes) {
+            System.out.print(node.data + " ");
+        }
+        System.out.println();
+
         for (int i = 0; i < matrix.length; i++) {
+            System.out.print(nodes.get(i).data + " ");
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
