e2b6db8720cee659434738cc5c890e338462ac38
davidcllm <davcm24@hotmail.com>
2026-05-17 23:41:35 -0600

    Cambio a Adjacency Matrix [Depth First Search]

--- dsaJava/src/Graph.java
+++ dsaJava/src/Graph.java
@@ -1,41 +1,41 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
 
 public class Graph {
-    ArrayList<LinkedList<Node>> alist;
-
-    Graph() {
-        alist = new ArrayList<>();
+    ArrayList<Node> nodes;
+    int[][] matrix;
+    Graph(int size) {
+        nodes = new ArrayList<>();
+        matrix = new int[size][size];
     }
 
     public void addNode(Node node) {
-        LinkedList<Node> currentList = new LinkedList<>();
-        currentList.add(node);
-        alist.add(currentList);
+        nodes.add(node);
     }
 
     public void addEdge(int src, int dst) {
-        LinkedList<Node> currentList = alist.get(src);
-        Node dstNode = alist.get(dst).get(0); //head of linkedlist
-        currentList.add(dstNode);
+        matrix[src][dst] = 1;
     }
 
     public boolean checkEdge(int src, int dst) {
-        LinkedList<Node> currentList = alist.get(src);
-        Node dstNode = alist.get(dst).get(0); //head of linkedlist
-
-        for (Node node : currentList) {
-            if (node == dstNode) {
-                return true;
-            }
+        if (matrix[src][dst] == 1) {
+            return true;
         }
-        return false;
+        else {
+            return false;
+        }
     }
 
     public void print() {
-        for (LinkedList<Node> currentList : alist) {
-            for (Node node : currentList) {
-                System.out.print(node.data + " -> ");
+        System.out.print("  ");
+        for (Node node : nodes) {
+            System.out.print(node.data + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.print(nodes.get(i).data + " ");
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
--- dsaJava/src/Main.java
+++ dsaJava/src/Main.java
@@ -2,14 +2,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        //Adjacency list: An array/arraylist of linkedlists. Each linkedl has a unique node at the head.
-        //All adjacent neighboors to that node are added to that node's linkedlisst.
+        //Depth First Search = Pick a route, keep going.
+        //If you reach a dead end, or an already visited node,
+        //backtrack to a previous node with unvisited adjacent neighbors.
 
-        //runtime complexity to check an edge: O(v)
-        //space complexity: O(v+e)
-
-        Graph graph = new Graph();
-
+        Graph graph = new Graph(5);
         graph.addNode(new Node('A'));
         graph.addNode(new Node('B'));
         graph.addNode(new Node('C'));
@@ -18,14 +15,11 @@ public class Main {
 
         graph.addEdge(0, 1);
         graph.addEdge(1, 2);
-        graph.addEdge(1, 4);
         graph.addEdge(2, 3);
         graph.addEdge(2, 4);
         graph.addEdge(4, 0);
         graph.addEdge(4, 2);
 
         graph.print();
-
     }
 }
-
